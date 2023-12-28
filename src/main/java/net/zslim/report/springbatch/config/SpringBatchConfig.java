482cf5773aff70b56c876ebb093d34ad8f6e44d2
limzisoon <limzisoon@hotmail.com>
2023-12-28 22:59:10 +0800

    comment field name

--- src/main/java/net/zslim/report/springbatch/config/SpringBatchConfig.java
+++ src/main/java/net/zslim/report/springbatch/config/SpringBatchConfig.java
@@ -80,41 +80,41 @@ public class SpringBatchConfig {
 
         FixedLengthTokenizer tokenizer = new FixedLengthTokenizer();
         Range[] ranges = new Range[] {
-                new Range(1, 3), //
-                new Range(4, 7), //
-                new Range(8, 11), //
-                new Range(12, 15), //
-                new Range(16, 19), //
-                new Range(20, 25), //
-                new Range(26, 27), //
-                new Range(28, 31), //
-                new Range(32, 37), //
-                new Range(38, 45), //
-                new Range(46, 48), //
-                new Range(49, 50), //
-                new Range(51, 51), //
-                new Range(52, 52), //
-                new Range(53, 62), //
-                new Range(63, 63), //
-                new Range(64, 73), //
-                new Range(74, 85), //
-                new Range(86, 86), //
-                new Range(87, 89), //
-                new Range(90, 101), //
-                new Range(102, 102), //
-                new Range(103, 105), //
-                new Range(106, 117), //
-                new Range(118, 118), //
-                new Range(119, 121), //
-                new Range(122, 129), //
-                new Range(130, 135), //
-                new Range(136, 141), //
-                new Range(142, 147), //
-                new Range(148, 162), //
-                new Range(163, 168), //
-                new Range(169, 175), //
-                new Range(176, 176), //
-                new Range(177, 303) //
+                new Range(1, 3), // RECORD CODE
+                new Range(4, 7), // CLIENT TYPE
+                new Range(8, 11), // CLIENT NUMBER
+                new Range(12, 15), // ACCOUNT NUMBER
+                new Range(16, 19), // SUBACCOUNT NUMBER
+                new Range(20, 25), // OPPOSITE PARTY CODE
+                new Range(26, 27), // PRODUCT GROUP CODE
+                new Range(28, 31), // EXCHANGE CODE
+                new Range(32, 37), // SYMBOL
+                new Range(38, 45), // EXPIRATION DATE
+                new Range(46, 48), // CURRENCY CODE
+                new Range(49, 50), // MOVEMENT CODE
+                new Range(51, 51), // BUY SELL CODE
+                new Range(52, 52), // QUANTITY LONG SIGN
+                new Range(53, 62), // QUANTITY LONG
+                new Range(63, 63), // QUANTITY SHORT SIGN
+                new Range(64, 73), // QUANTITY SHORT
+                new Range(74, 85), // BROKER FEE
+                new Range(86, 86), // BROKER FEE DC
+                new Range(87, 89), // BROKER FEE CUR CODE
+                new Range(90, 101), // CLEARING FEE
+                new Range(102, 102), // CLEARING FEE DC
+                new Range(103, 105), // CLEARING FEE CUR CODE
+                new Range(106, 117), // COMMISSION
+                new Range(118, 118), // COMMISSION DC
+                new Range(119, 121), // COMMISSION CUR CODE
+                new Range(122, 129), // TRANSACTION DATE
+                new Range(130, 135), // FUTURE REFERENCE
+                new Range(136, 141), // TICKET NUMBER
+                new Range(142, 147), // EXTERNAL NUMBER
+                new Range(148, 162), // TRANSACTION PRICE
+                new Range(163, 168), // TRADER INITIALS
+                new Range(169, 175), // OPPOSITE TRADER ID
+                new Range(176, 176), // OPEN CLOSE CODE
+                new Range(177, 303) // FILLER
         };
         tokenizer.setColumns(Arrays.asList(ranges).toArray(new Range[ranges.length]));
         tokenizer.setNames(
